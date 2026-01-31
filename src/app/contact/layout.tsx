1e691296493e01868c0ee907d8d4e00531b83278
Evade Won <akashsingh926369@gmail.com>
2026-01-31 16:17:28 +0530

    added children type in contact layout

--- src/app/contact/layout.tsx
+++ src/app/contact/layout.tsx
@@ -1,6 +1,6 @@
 import Navbar from "@/components/Navbar"
 
-const layout = ({ children }) => {
+const layout = ({ children}:{children:React.ReactNode}) => {
     return (
         <div className="relative min-h-screen text-[#e8e6e6]" style={{ backgroundImage: "url('/background.png')", }}>
             <Navbar />
