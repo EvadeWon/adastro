bc1828361d0e8db1e2fbde5d94dd2d60e7b7c480
Evade Won <akashsingh926369@gmail.com>
2026-02-07 13:44:50 +0530

    fix : google o auth bug

--- src/app/api/auth/google/route.ts
+++ src/app/api/auth/google/route.ts
@@ -1,12 +1,12 @@
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request) {
-    const CLIENT_URL = process.env.NODE_ENV === 'production'
-        ? `https://${process.env.VERCEL_URL}`
-        : process.env.CLIENT_URL;
-    // Redirect to Google OAuth
-    const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=${CLIENT_URL}/api/auth/google/callback&response_type=code&scope=profile%20email`;
+    const CLIENT_URL =
+        process.env.NODE_ENV === "production"
+            ? process.env.BASE_URL
+            : process.env.CLIENT_URL;
 
+    const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=${CLIENT_URL}/api/auth/google/callback&response_type=code&scope=profile%20email`;
 
     return NextResponse.redirect(authUrl);
 }
