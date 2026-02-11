87af66528b2ca9e8d7a7dabc3ad7a34deb2da409
Evade Won <akashsingh926369@gmail.com>
2026-02-11 21:32:58 +0530

    fix : bugs enrollment

--- src/app/api/check-enrollment/route.ts
+++ src/app/api/check-enrollment/route.ts
@@ -1,30 +1,30 @@
-// import { connectDB } from "@/dbConfig/dbConfig";
+import { connectDB } from "@/dbConfig/dbConfig";
 // import Purchase from "@/models/purchase";
 // import User from "@/models/userModel";
 // import { authOptions } from "@/pages/api/auth/[...nextauth]";
 // import { getServerSession } from "next-auth/next";
 
-// export async function GET(req: Request) {
-//     await connectDB();
+export async function GET(req: Request) {
+    await connectDB();
 
-//     const session = await getServerSession({ req, ...authOptions });
-//     if (!session?.user?.email) {
-//         return new Response(JSON.stringify({ enrolled: false }), { status: 401 });
-//     }
+    // const session = await getServerSession({ req, ...authOptions });
+    // if (!session?.user?.email) {
+    //     return new Response(JSON.stringify({ enrolled: false }), { status: 401 });
+    // }
 
-//     const url = new URL(req.url);
-//     const courseId = url.searchParams.get("courseId");
-//     if (!courseId) return new Response(JSON.stringify({ message: "courseId is required" }), { status: 400 });
+    const url = new URL(req.url);
+    const courseId = url.searchParams.get("courseId");
+    if (!courseId) return new Response(JSON.stringify({ message: "courseId is required" }), { status: 400 });
 
-//     const user = await User.findOne({ email: session.user.email });
-//     if (!user) return new Response(JSON.stringify({ enrolled: false }), { status: 404 });
+    // const user = await User.findOne({ email: session.user.email });
+    // if (!user) return new Response(JSON.stringify({ enrolled: false }), { status: 404 });
 
-//     // ✅ Check in Purchase collection
-//     const purchase = await Purchase.findOne({
-//         userId: user._id,
-//         courseId: Number(courseId),
-//         status: "PAID",
-//     });
+    // ✅ Check in Purchase collection
+    // const purchase = await Purchase.findOne({
+    //     userId: user._id,
+    //     courseId: Number(courseId),
+    //     status: "PAID",
+    // });
 
-//     return new Response(JSON.stringify({ enrolled: !!purchase }), { status: 200 });
-// }
+    // return new Response(JSON.stringify({ enrolled: !!purchase }), { status: 200 });
+}
