076cb4f7844527747f877ae7be39916522cf2f11
Evade Won <akashsingh926369@gmail.com>
2026-02-14 16:30:11 +0530

    added revenue chart

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,6 +2,7 @@
 import BrandsTicker from "@/components/BrandsTicker"
 import CoursesSection from "@/components/Courses"
 import Header from "@/components/Header"
+import RevenueChart from "@/components/RevenueChart"
 import ServiceSection from "@/components/Services"
 import { useRef } from "react"
 
@@ -13,6 +14,14 @@ const Home = () => {
       <Header courseRef={courseRef} serviceRef={serviceRef} />
       <div className="border-t border-b border-[#571e0ac9]">
         <BrandsTicker />
+      </div>
+      <div className="p-10 sm:p-30 min-h-screen">
+        <h2 className="text-xl md:text-3xl font-bold mb-6 text-center uppercase">
+          Monthly Revenue Growth
+        </h2>
+        <div className="p-10 rounded-xl shadow-lg">
+          <RevenueChart />
+        </div>
       </div>
       <div ref={serviceRef}>
         <ServiceSection />
---
+++ src/components/RevenueChart.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import {
+    BarElement,
+    CategoryScale,
+    Chart as ChartJS,
+    Legend,
+    LinearScale,
+    Title,
+    Tooltip,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
+export default function RevenueChart() {
+    const data = {
+        labels: ["Sep 2025", "Oct 2025", "Nov 2025", "Dec 2025", "Jan 2026","Feb 2026"],
+        datasets: [
+            {
+                label: "Monthly Revenue",
+                data: [1658453, 1959063, 2830980, 2921715, 4232866,1686765],
+                backgroundColor: "#ef4444",
+                borderRadius: 8,
+            },
+        ],
+    };
+
+    const options: any = {
+        responsive: true,
+        plugins: {
+            legend: {
+                display: false,
+            },
+        },
+        scales: {
+            y: {
+                beginAtZero: true,
+            },
+        },
+    };
+
+    return <Bar data={data} options={options} />;
+}
